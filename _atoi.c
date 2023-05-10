4418f43a6b00b970fb86f8b776cf27685cc6afc1
itzsinox <jabar.yassine10@gmail.com>
2023-05-10 08:48:31 -0700

    up

--- _atoi.c
+++ _atoi.c
@@ -1,19 +1,20 @@
-#include "shell.h"
 
+/**
+ * interactive - intr mode
+ * @info: struct
+ * Return: 1 or 0
+ */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
 
-
+/**
+ * _isalpha - to check alpha
+ * @c: car input
+ * Return: 1 or 0
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -22,31 +23,49 @@ int _isalpha(int c)
 		return (0);
 }
 
+/**
+ * is_delim - to check delimeter
+ * @c: the char
+ * @delim: delim str
+ * Return: 1 or 0
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
 
+/**
+ * _atoi - convert str to int
+ * @s: str
+ * Return: 0 or converted num
+ */
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+	int i, sg = 1, f = 0, op;
+	unsigned int res = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && f != 2; i++)
 	{
 		if (s[i] == '-')
-			sign *= -1;
+			sg *= -1;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			f = 1;
+			res *= 10;
+			res += (s[i] - '0');
 		}
-		else if (flag == 1)
-			flag = 2;
+		else if (f == 1)
+			f = 2;
 	}
 
-	if (sign == -1)
-		output = -result;
+	if (sg == -1)
+		op = -res;
 	else
-		output = result;
+		op = res;
 
-	return (output);
+	return (op);
 }
