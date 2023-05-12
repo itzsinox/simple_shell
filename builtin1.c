78f29b558b7ea2e4b987f9445b94a432ed00b597
ABIR-MOUADNI-44 <mouadniabir44@gmail.com>
2023-05-12 13:51:20 -0700

    Update

---
+++ builtin1.c
@@ -0,0 +1,110 @@
+#include "shell.h"
+
+/**
+ * unset_alias - set alias
+ * @info: struct
+ * @str: str
+ * Return: 0 or 1
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *x, y;
+	int r;
+
+	x = _strchr(str, '=');
+
+	if (!x)
+		return (1);
+	y = *x;
+	*x = 0;
+	r = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*x = y;
+	return (r);
+}
+
+/**
+    * _myhistory – to display the history list
+    * @info: Structure
+    * Return: 0
+*/
+int _myhistory(info_t *info)
+{
+	print_list(info->history);
+	return (0);
+}
+
+/**
+    * print_alias – print string
+    * @node: node
+    * Return: 0 or 1
+*/
+int print_alias(list_t *node)
+{
+	char *x = NULL, *y = NULL;
+
+	if (node)
+	{
+		x = _strchr(node->str, '=');
+		for (y = node->str; y <= x; y++)
+		_putchar(*y);
+		_putchar('\'');
+		_puts(x + 1);
+		_puts("'\n");
+		return (0);
+}
+return (1);
+}
+
+/**
+    * set_alias – set string
+    * @info: struct
+    * @str: str
+    * Return: 0 or 1
+*/
+int set_alias(info_t *info, char *str)
+{
+	char *x;
+
+	x = _strchr(str, '=');
+	if (!x)
+		return (1);
+	if (!*++x)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+    * _myalias - alias
+    * @info: Structure
+    * Return: 0
+*/
+int _myalias(info_t *info)
+{
+	int x = 0;
+	char *y = NULL;
+	list_t *n = NULL;
+
+	if (info->argc == 1)
+	{
+		n = info->alias;
+		while (n)
+		{
+			print_alias(n);
+			n = n->next;
+		}
+		return (0);
+	}
+	for (x = 1; info->argv[x]; x++)
+	{
+		y = _strchr(info->argv[x], '=');
+		if (y)
+			set_alias(info, info->argv[x]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[x], '='));
+	}
+
+	return (0);
+}
---
+++ getenv.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+    * get_environ - returns str arr
+    * @info: Structure
+    * Return: 0
+*/
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+    * _unsetenv - Remove environment variable
+    * @info: Structure
+    * @var: str env var
+    * Return: 1 or 0
+*/
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
